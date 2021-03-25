b734572e6150afe317d6a69534973646f605c4c0
Truong <hntruong@lacviet.com.vn>
2021-03-25 20:19:06 +0700

    add file main.java

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import static java.lang.System.out;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        out.print("hello word");
+        //1: tao thu muc .git--> git init
+        //2: git status: xem trang thai folder
+        //3: git add out/ : luu file out/,  git add .: luu all
+    }
+}
