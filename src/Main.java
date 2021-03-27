980f71cd18b38e8f31c26dc021df3d1c4331955f
Truong <hntruong@lacviet.com.vn>
2021-03-27 20:18:40 +0700

    test

--- src/Main.java
+++ src/Main.java
@@ -2,28 +2,13 @@ import static java.lang.System.out;
 
 public class Main {
     public static void main(String[] args) throws Exception{
-        //out.print("hello word");
-        //1: tao thu muc .git--> git init
-        //2: git status: xem trang thai hay sự thay dổi j(file nao thay đổi chua checkin)
-        //3: git add out/ : luu file out/,  git add .: luu all-- lưu trữ sự thay đổi
-        //4: commit: checkin --> git commit -m "ghi chu" : "checkin lên local repo"  nếu lỗi User commit sẽ thực hiện bước 5
-        //5: git config --global user.name "First_name last_name"
-        //   git config --global user.email "My_Name@example.com"
-        //6: git log: cho biết các commit đã thực hiện lưu trữ trong git (history): có thể undo lại các lần checkin trước(get last tfs)
-//        String, int, Boolean, double, float, char,…
-//        Nhóm kiểu dữ liệu nguyên thủy: ko hổ trợ phương thức xử lý(viết thường hết: string,int)
-//        Nhóm kiểu dữ lieu đối tượng: hổ trợ phương thức xử lý(CHữ cái đầu viết hoa: String, Int)
+        int a=5,b=10,ketqua=0;
+        if(a>b) ketqua=1;
+        else if(a<b) ketqua = -1;
+        else ketqua=0;
+        //out.print(ketqua);
 
-        //I.Khai báo biến
-        String name = "Ho nhat truong";
-        float age = 15;
-        char ss= 'a';
-        //II.Toán tử
-        //   Số: ** là lũy thừa, // là căn
-        //4+4-4-4+4-4
-        int a = 5;
-        int b=5;
-        int kq = a-- + --b - --b - --a + b-- - --a;
-        out.print(kq);
+        ketqua = a>b?1:a<b?-1:0;
+        out.print(ketqua);
     }
 }
